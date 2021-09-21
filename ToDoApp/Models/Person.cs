c1f823b6656b9b1f97b2d2ea8c8833555c81205b
rashin aeini <aeini.rashin@gmail.com>
2021-09-21 20:01:15 +0200

    Create Person Class

---
+++ ToDoApp/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class Person
+    {
+        private readonly int personId;
+        private string firstName;
+        private string lastName;
+
+
+        public int PersonId { get { return personId; } }
+        public string FirstName { get { return firstName; } set {firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+
+        public Person(int id)
+        {
+            personId = id;
+            
+        }
+    }
+}
