d85868eb61c33f6ecd239da043efe6b1aa608842
rashin aeini <aeini.rashin@gmail.com>
2021-09-22 14:54:14 +0200

    Complete Todo Sequencer

---
+++ ToDoApp/Data/TodoSequencer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Data
+{
+    public static class TodoSequencer
+    {
+        private static int todoId;
+        public static int nextTodoId()
+        {
+            return ++todoId;
+        }
+        public static void reset()
+        {
+            todoId = 0;
+        }
+
+    }
+}
