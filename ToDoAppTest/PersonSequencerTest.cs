a5ec0cabba1587f68749a2968b4b24adf2fa83fb
rashin aeini <aeini.rashin@gmail.com>
2021-09-22 14:48:00 +0200

    Complete PersonSequencer Test

---
+++ ToDoAppTest/PersonSequencerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Data;
+using ToDoApp.Models;
+using Xunit;
+
+namespace ToDoAppTest
+{
+   public class PersonSequencerTest
+    {
+        [Fact]
+        public void PersonIdTest()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                PersonSequencer.nextPersonId();
+            }
+
+            Assert.Equal(10, PersonSequencer.nextPersonId());
+           
+        }
+        [Fact]
+        public void ResetTest()
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                PersonSequencer.nextPersonId();
+
+            }
+            PersonSequencer.reset();
+            Assert.Equal(1, PersonSequencer.nextPersonId());
+
+        }
+    }
+}
