19761512f4e388c902e335279d525f1761c85d3b
rashin aeini <aeini.rashin@gmail.com>
2021-09-23 20:23:25 +0200

    complete people Test

---
+++ ToDoAppTest/PeopleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Data;
+using ToDoApp.Models;
+using Xunit;
+
+namespace ToDoAppTest
+{
+    public class PeopleTest
+    {
+        [Fact]
+        public void FindbyIdTest()
+        {
+            Person find = People.FindById(1);
+            Assert.Equal(null,find );
+
+        }
+
+    }
+}
