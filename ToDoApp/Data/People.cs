8abe5add391e575d2457e2ea4c358e35e86521fa
rashin aeini <aeini.rashin@gmail.com>
2021-09-23 20:23:00 +0200

    complete people

---
+++ ToDoApp/Data/People.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
+{
+    public static class People
+    {
+        private static Person[] personArray = new Person[0];
+
+        public static int Size()
+        {
+            return personArray.Length;
+        }
+        public static Person[] FindAll()
+        {
+            return personArray;
+        }
+        public static Person FindById(int personId)
+        {
+            foreach (var item in personArray)
+            {
+                if (item.PersonId== personId)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+
+        public static void Add(Person entry)
+        {
+
+            Array.Resize(ref personArray, Size() + 1); // bozorg kardane yek araye
+            personArray[Size() - 1] = entry; //shomare andis araye
+
+        }
+
+
+        public static void Clear()
+        {
+            personArray = new Person[0];
+
+        }
+    }
+}
