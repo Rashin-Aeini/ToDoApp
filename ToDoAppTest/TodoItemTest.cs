b6927617e96d17590186c22cd3a50f3ae45452d2
rashin aeini <aeini.rashin@gmail.com>
2021-09-26 15:52:25 +0200

    Pass the test todoItems

---
+++ ToDoAppTest/TodoItemTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Data;
+using ToDoApp.Models;
+using Xunit;
+
+namespace ToDoAppTest
+{
+    public class TodoItemTest
+    {
+        [Fact]
+        public void sizeTest()
+        {
+            TodoItems.Clear();
+            Assert.Equal(0, TodoItems.Size());
+        }
+
+        [Fact]
+        public void UnassignedTest()
+        {
+            TodoItems.Clear();
+            TodoItems.Add(new Todo(0, ""));
+            Assert.Equal(1, TodoItems.FindUnassignedTodoItems().Length);
+
+        }
+    }
+}
